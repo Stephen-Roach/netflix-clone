f4b9bf0ac09417ae87cf7008c285e481973447c3
Stephen-Roach <stephenroach21@gmail.com>
2021-02-04 23:08:50 -0600

    completed main ui and tmdb api

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBOrdB2zUPM7DAjQe3F0d4IjpE6VW3tolA',
+  authDomain: 'netflix-clone-41716.firebaseapp.com',
+  projectId: 'netflix-clone-41716',
+  storageBucket: 'netflix-clone-41716.appspot.com',
+  messagingSenderId: '84055621277',
+  appId: '1:84055621277:web:8d640497bcefd89c009e74',
+};
