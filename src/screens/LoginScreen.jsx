bcd2419dd74007c014143773cbbbdae3cd8f8263
Stephen-Roach <stephenroach21@gmail.com>
2021-02-21 13:24:38 -0600

    created signin button

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -10,6 +10,7 @@ function LoginScreen() {
           src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
           alt=''
         />
+        <button className='loginScreen__button'>Sign In</button>
       </div>
     </div>
   );
