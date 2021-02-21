0eef278971c031077beccd6876619f46f5a80b28
Stephen-Roach <stephenroach21@gmail.com>
2021-02-21 13:33:59 -0600

    added background gradient

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -11,6 +11,8 @@ function LoginScreen() {
           alt=''
         />
         <button className='loginScreen__button'>Sign In</button>
+
+        <div className='loginScreen__gradient' />
       </div>
     </div>
   );
