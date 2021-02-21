dd7d381f7830e1cb649edbd3087288ddaafb54eb
Stephen-Roach <stephenroach21@gmail.com>
2021-02-21 14:53:36 -0600

    added login body and email input

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -14,6 +14,24 @@ function LoginScreen() {
 
         <div className='loginScreen__gradient' />
       </div>
+
+      <div className='loginScreen__body'>
+        <>
+          <h1>Unlimited films, TV programmes and more.</h1>
+          <h2>Watch anywhere. Cancel at an time.</h2>
+          <h3>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </h3>
+
+          <div className='loginScreen__input'>
+            <form>
+              <input type='email' placeholder='Email Address' />
+              <button className='loginScreen__getStarted'>Get Started</button>
+            </form>
+          </div>
+        </>
+      </div>
     </div>
   );
 }
