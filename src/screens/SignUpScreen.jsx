28e62d893f535eea650098aad2ecd5d6d2e3776b
Stephen-Roach <stephenroach21@gmail.com>
2021-02-21 22:08:59 -0600

    created signup page

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import './SignUpScreen.css';
 
-function SignInScreen() {
-  return <div></div>;
+function SignUpScreen() {
+  return (
+    <div className='signupScreen'>
+      <form>
+        <h1>Sign In</h1>
+        <input type='email' placeholder='Email' />
+        <input type='password' placeholder='Password' />
+        <button type='submit'>Sign In</button>
+        <h4>
+          <span className='signupScreen__gray'>New to Netflix? </span>
+          <span className='signupScreen__link'>Sign Up now.</span>
+        </h4>
+      </form>
+    </div>
+  );
 }
 
-export default SignInScreen;
+export default SignUpScreen;
